1aa2a66b8bc992fffe4ab6a7419fc2ced844ba69
Julian Khatibi <ajkhatibi@gmail.com>
2020-08-05 15:39:31 -0700

    added tab bar icon

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import FeedsScreen from './screens/FeedsScreen/FeedsScreen.tsx';
 import UsersScreen from './screens/UsersScreen/UsersScreen.tsx';
 import TodosScreen from './screens/TodosScreen/TodosScreens.tsx';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
+
+const _screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+    if (route.name === "Users") {
+      iconName = 'users';
+    } else if (route.name === "Feeds") {
+      iconName = 'rss';
+    } else if (route.name === "Todos") {
+      iconName = "check-square";
+    }
+
+    // You can return any component that you like here!
+    return <Icon name={iconName} size={size} color={color} />;
+  },
+})
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Feeds" component={FeedsScreen} />
+      <Tab.Navigator
+        screenOptions={_screenOptions}
+      >
         <Tab.Screen name="Users" component={UsersScreen} />
+        <Tab.Screen name="Feeds" component={FeedsScreen} />
         <Tab.Screen name="Todos" component={TodosScreen} />
       </Tab.Navigator>
     </NavigationContainer>
